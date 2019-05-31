d03a67afdd63fe8c015a17e6b9a6f528be8e0188
morswin22 <xmorswinx@gmail.com>
2019-05-31 20:31:54 +0200

    Adding reload mechanics

--- js/generator.js
+++ js/generator.js
@@ -8,4 +8,16 @@ function generate() {
     paragraph.innerHTML = quote;
 }
 
+window.addEventListener('keyup', function(e){
+    if (e.keyCode == 82) {
+        generate();
+    }
+});
+
+// dblclick doesn't work on mobile try something like https://stackoverflow.com/a/27561006
+document.querySelector('img').addEventListener('dblclick', function(e){
+    generate();
+    e.preventDefault();
+});
+
 generate();
